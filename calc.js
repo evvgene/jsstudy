d4cc530f44b1853236f675b0448665614be01987
evvgene <evvgene@gmail.com>
2018-10-19 14:59:32 +0400

    calc

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add() //addition
+{
+    res.value=num1.value-(-1)*num2.value;
+}
+
+function sub() //subtraction
+{
+    res.value=num1.value-num2.value;
+}
+
+function mult() //multiplication
+{
+    res.value=num1.value*num2.value;
+}
+
+function div() //division
+{
+    res.value=num1.value/num2.value;
+}
